59cc5ebfe30e4afea74ea7c0e929405963150e41
David Basile Filho <davidbasilefilho@gmail.com>
2023-03-05 15:35:40 -0300

    added nvchad

---
+++ .config/nvchad/chadrc.lua
@@ -0,0 +1,45 @@
+-- First read our docs (completely) then check the example_config repo
+
+local M = {}
+
+M.ui = {
+  theme = "catppuccin",
+}
+
+M.plugins = {
+  -- Enabling Plugins
+  ["goolord/alpha-nvim"] = { disable = false },
+  ["folke/which-key.nvim"] = { disable = false },
+
+  ["williamboman/mason.nvim"] = {
+    override_options = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+        "emmet-ls",
+        "json-lsp",
+        "prettier",
+
+        -- my stuff
+        "csharp-language-server",
+        "csharpier",
+        "clangd",
+        "clang-format",
+
+        -- shell
+        "shfmt",
+        "shellcheck",
+      },
+    },
+  }
+
+}
+
+return M
