cfa509ebc9ecb6c03dbb87fb835b43f879961410
David Basile Filho <davidbasilefilho@gmail.com>
2023-03-12 17:41:49 -0300

    switched jamesdsp for easyeffects

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,5 +2,6 @@
 picom --experimental-backends --config $HOME/.config/picom/picom.conf &
 /usr/bin/emacs --daemon &
 variety &
+easyeffects --gapplication-service &
 #conky -c $HOME/.config/conky/qtile/doom-one-01.conkyrc
 #volumeicon & nm-applet &
