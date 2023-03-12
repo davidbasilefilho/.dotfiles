1f7ab0c5ea2fa80581f0eb8d9c4011c1e74e2e16
David Basile Filho <davidbasilefilho@gmail.com>
2023-03-12 11:43:13 -0300

    Added dependencies

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,6 @@ alias cp='cp -i'
 alias ls='exa -la'
 alias em='emacs -nw'
 
-alias emacsclient="emacsclient -c -a 'emacs'"
-
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib64"
 
 export PATH="$HOME/.emacs.d/bin:$PATH"
@@ -18,6 +16,6 @@ export PATH="$HOME/.dotfiles/bin:$PATH"
 
 set -U fish_greeting
 
-# oh-my-posh init fish --config /home/basile/.config/oh-my-posh/star.omp.json | source
 starship init fish | source
+
 fastfetch
