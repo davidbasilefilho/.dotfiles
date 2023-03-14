4dfc59c2169defe0f21fb71d133166ac5bd6fc42
David Basile Filho <39467981+DavidBF16@users.noreply.github.com>
2023-03-14 10:26:05 -0300

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-picom --experimental-backends --config $HOME/.config/picom/picom.conf &
+#picom --experimental-backends --config $HOME/.config/picom/picom.conf &
 /usr/bin/emacs --daemon &
-variety &
+#variety &
 easyeffects --gapplication-service &
 #conky -c $HOME/.config/conky/qtile/doom-one-01.conkyrc
 #volumeicon & nm-applet &
